2561bb04a74a1a60819684a6d08f3b4cb762d5a5
nhatminh-07 <tranngocnhatminh07@gmail.com>
2024-08-17 21:43:41 +0700

    update

--- maytinh.py
+++ maytinh.py
@@ -4,15 +4,18 @@ from tkinter import *
 #tao man hinh tinh
 window = tkinter.Tk()
 
-
 #thiet lap tieu de
 window.title("May tinh bo tui")
 
 e = Entry(width =20, borderwidth=10, font = "Arial 18") # hộp nhập văn bản
-e.grid(row = 0, column = 0, columnspan = 4, pady = 10)
+e.grid(row = 0, column = 0, columnspan = 5, pady = 10)
+
+
+def click():
+    e.insert(0, "1")
 
 # các nút bấm
-bt_1 = Button(text = "1", width = 10, height = 3)
+bt_1 = Button(text = "1", width = 10, height = 3, command = click())
 bt_1.grid(row = 1, column = 0)
 bt_2 = Button(text = "2", width = 10, height = 3)
 bt_2.grid(row = 1, column = 1)
@@ -26,6 +29,10 @@ bt_5 = Button(text = "5", width = 10, height = 3)
 bt_5.grid(row = 2, column = 1)
 bt_6 = Button(text = "6", width = 10, height = 3)
 bt_6.grid(row = 2, column = 2)
+bt_mul = Button(text = "x", width = 10, height = 3)
+bt_mul.grid(row = 2, column = 3)
+bt_div = Button(text = ":", width = 10, height = 3)
+bt_div.grid(row = 2, column = 4)
 
 # H3
 bt_7 = Button(text = "7", width = 10, height = 3)
@@ -34,6 +41,10 @@ bt_8 = Button(text = "8", width = 10, height = 3)
 bt_8.grid(row = 3, column = 1)
 bt_9 = Button(text = "9", width = 10, height = 3)
 bt_9.grid(row = 3, column = 2)
+bt_pl = Button(text = "+", width = 10, height = 3)
+bt_pl.grid(row = 3, column = 3)
+bt_min = Button(text = "-", width = 10, height = 3)
+bt_min.grid(row = 3, column = 4)
 
 # H0
 bt_0 = Button(text = "0", width = 10, height = 3)
