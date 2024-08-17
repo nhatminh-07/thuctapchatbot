9b1c0ec2b3030d24fb0e5fac940f7d52fe63a8ea
nhatminh-07 <tranngocnhatminh07@gmail.com>
2024-08-17 21:08:56 +0700

    update

--- maytinh.py
+++ maytinh.py
@@ -1,4 +1,5 @@
 import tkinter
+from tkinter import *
 
 #tao man hinh tinh
 window = tkinter.Tk()
@@ -7,5 +8,14 @@ window = tkinter.Tk()
 #thiet lap tieu de
 window.title("May tinh bo tui")
 
+e = Entry(width =20, borderwidth=10, font = "Arial 18") # hộp nhập văn bản
+e.grid()
+
+
 #Luôn để xuống cuối dòng
 window.mainloop()
+#giống matplotlib
+#mục tiêu tạo ra textbox này!!
+
+# 24/5/2020, Tkinter
+
