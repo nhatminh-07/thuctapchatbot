d18098b805fe34df2e1b05a76c1efd9ac837c205
nhatminh-07 <tranngocnhatminh07@gmail.com>
2024-08-24 22:24:21 +0700

    update

---
+++ chatbot/computer_response.py
@@ -0,0 +1,7 @@
+import tkinter
+import tkinter as tk
+
+def response(text):
+    if "hello" in text:
+        computer_text = "hi, nice to meet you"
+    
--- chatbot/main.py
+++ chatbot/main.py
@@ -2,6 +2,7 @@
 
 import tkinter
 import tkinter as tk
+import computer_response
 
 window = tk.Tk()
 window.title("Chatbot")
@@ -11,11 +12,10 @@ def send_text():
     user_text = text_box.get('1.0', tk.END)
     text_area.insert(tk.END, "You: " + user_text)
     text_box.delete("1.0", tk.END)
-    computer_response(user_text)
+    computer_responsei(user_text)
 
-def computer_response(text):
-    if "hello" in text:
-        computer_text = "hi, nice to meet you"
+def computer_responsei(user_text):
+    computer_text = computer_response.response(user_text)
     text_area.insert(tk.END, "Computer: " + computer_text)
 
 # Ô đối thoạik
@@ -25,7 +25,7 @@ text_area = tk.Text(width =40, height = 20)
 text_box = tk.Text(width = 40, height = 2)
 
 # nút Clear
-bt_clear = tk.Button(width = 10, height = 2, text = "Clear", command = lambda: text_area.delete(0, tk.END))
+bt_clear = tk.Button(width = 10, height = 2, text = "Clear", command = lambda: text_area.delete("1.0", tk.END))
 
 # nút send
 bt_send = tk.Button(width = 10, height = 2, text = "send", command = lambda: send_text())
